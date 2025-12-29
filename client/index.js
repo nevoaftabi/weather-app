b95620c11d9adc9a42a76d8b18a3a01eb2727cde
Nevo Aftabi <curryboy1245@gmail.com>
2025-12-29 11:13:04 -0500

    Added rate limiting

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ const getDataAndCall = async () => {
   const state = document.getElementById("state").value.trim();
   const resultEl = document.getElementById("result");
   const weatherEl = document.getElementById("weather-condition");
+  const units = document.getElementById("units").textContent;
 
   if (!city || !state) {
     resultEl.textContent = "Enter both city and state (e.g., Miami, FL).";
@@ -16,7 +17,7 @@ const getDataAndCall = async () => {
 
   try {
     const res = await fetch(
-      `http://localhost:3000/api/weather?city=${city}&state=${state}`
+      `http://localhost:3000/api/weather?city=${city}&state=${state}&units=${units}`
     );
 
     // If server returns 400/404/500, don’t try to read weather.main.temp
@@ -27,11 +28,20 @@ const getDataAndCall = async () => {
       resultEl.textContent = msg;
       return;
     }
+    console.log(data);
 
     // Now safe to read
-    resultEl.textContent = `${data.main.temp}°C (feels like ${data.main.feels_like}°C)`;
-    weatherEl.textContent = data.weather?.[0]?.description ?? "";
-  } catch (err) {
+    if(units === "Imperial") {
+      resultEl.textContent = `${data.temp}°F (feels like ${data.feelsLike}°F)`;
+    }
+    else {
+
+      resultEl.textContent = `${data.temp}°C (feels like ${data.feelsLike}°C)`;
+    }
+
+    weatherEl.textContent = data.condition ?? "";
+  } 
+  catch (err) {
     console.error(err);
     resultEl.textContent = err.message || "Something went wrong.";
   }
